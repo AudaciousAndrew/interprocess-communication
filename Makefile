9708e7d4b9ed28b34907ef22e806fbcce0b8f404
AudaciousAndrew <pukamirka@gmail.com>
2019-04-11 21:55:21 +0300

    log files creating function

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 CC=gcc
 CFLAGS=-std=c99 -Wall -pedantic 
 
-all: 
+all: clean
 	$(CC) $(CFLAGS) main.c -o main
 
+
+clean:
+	rm *.log
--- main.c
+++ main.c
@@ -8,13 +8,35 @@
 
 int n;
 
+
 int get_args(int argc, char **argv) {
 	if (argc != 3 || (strcmp(argv[1], "-p") != 0)) 	return -1;
 	n = atoi(argv[2]);
 	return 0;
 }
 
+int create_logs(FILE *pipes_log , FILE *events_log){
+	pipes_log = fopen("pipes.log","w");
+	if(pipes_log == NULL){
+		 perror("can't open/create pipes.log file (fopen)");
+		return -1;
+	}
+	events_log = fopen("events.log" , "w+");
+	if(events_log == NULL){
+		 perror("can't open/create events.log file (fopen)");
+		return -1;
+	}
+	return 0;
+}
+
 int main(int argc, char **argv) {
+	
+	FILE *pipes_log = NULL;
+	FILE *events_log = NULL;
+
 	if(!get_args(argc, argv)) return -1;
+	
+	if ( create_logs(pipes_log , events_log) < 0) return -1;
+	
 	return 0;
 }
